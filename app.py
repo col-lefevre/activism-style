a254ca60e9546f53a51f076d3a50af51df998208
clefevr <clefevr@iu.edu>
2022-12-21 13:12:12 -0500

    v0.5

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
+app.secret_key = 'prohealth'
 
 @app.route("/")
 def index():
+    session["x"] = "test"
+    return render_template('index.html', page_css=url_for('static', filename='css/index.css'), page_title='Home')
+
+@app.route("/test")
+def test():
+    print(session["x"])
     return render_template('index.html', page_css=url_for('static', filename='css/index.css'), page_title='Home')
