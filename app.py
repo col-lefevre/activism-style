931094d8a8b640d876930bd2134bf3886e86731e
clefevr <clefevr@iu.edu>
2022-12-20 22:49:57 -0500

    v0.2

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('base.html', page_css=url_for('static', filename='css/index.html'))
+    return render_template('base.html', page_css=url_for('static', filename='css/index.css'))
