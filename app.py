1f5884b04424a8ba3200f98655ad1bc8c7c5a0e2
clefevr <clefevr@iu.edu>
2022-12-20 20:53:57 -0500

    v0.1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template('base.html', page_css=url_for('static', filename='css/index.html'))
